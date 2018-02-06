d08dd4ec8df9d875d208b985d2e567fc9b5fd11b
rndev80 <jianmyphy123@gmail.com>
2018-02-06 19:50:39 +0100

    apply react-hot-module

--- bitimulate-frontend/src/index.js
+++ bitimulate-frontend/src/index.js
@@ -4,6 +4,21 @@ import 'styles/main.scss';
 import Root from 'Root';
 import registerServiceWorker from './registerServiceWorker';
 import store from 'store';
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(<Root store={store}/>, document.getElementById('root'));
+const render = (Component) => ReactDOM.render(
+  (
+    <AppContainer>
+      <Component store={store}/>
+    </AppContainer>
+  ),
+  document.getElementById('root')
+);
+
+render(Root);
+
+if(module.hot) {
+  module.hot.accept('./Root', () => render(Root));
+}
+
 registerServiceWorker();

cf03cb42062f359ce8cdab4cb0b10b7c0c003353
rndev80 <jianmyphy123@gmail.com>
2018-02-06 20:12:49 +0100

    fixed react-hot-loader & login modal

--- bitimulate-frontend/src/components/atoms/Button/Button.js
+++ bitimulate-frontend/src/components/atoms/Button/Button.js
@@ -4,17 +4,49 @@ import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
-const Button = ({children, roundCorner, invert, className, ...rest}) => {
+const Button = ({
+  children,
+  flex,
+  className,
+  roundCorner,
+  invert,
+  flat,
+  color,
+  padding="0.5rem",
+  xPadding,
+  style,
+  disabled,
+  dark,
+  onClick,
+  theme,
+  ...rest
+}) => {
+  const dynamicStyle = {
+    ...(xPadding ? {
+      paddingLeft: xPadding,
+      paddingRight: xPadding
+    } : {})
+  }
   return (
     <div className={
-        cx('button', {
-          'round-corner': roundCorner,
-          invert
-        }, className)
-      } {...rest}>
+      cx('button', {
+        invert,
+        flex,
+        flat,
+        disabled,
+        dark,
+      }, color, className, theme)
+    }
+    style={{
+      padding,
+      ...style,
+      ...dynamicStyle
+    }}
+    onClick={disabled ? undefined : onClick}
+     {...rest}>
       {children}
     </div>
   );
-}
+};
 
 export default Button;
--- bitimulate-frontend/src/components/organisms/LoginModal/LoginModal.js
+++ bitimulate-frontend/src/components/organisms/LoginModal/LoginModal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './LoginModal.scss';
 import classNames from 'classnames/bind';
-import { Modal, Input } from 'components';
+import { Modal, Input, Button, TextButton } from 'components';
 
 
 const cx = classNames.bind(styles);
@@ -12,11 +12,30 @@ const LoginModal = ({visible}) => {
       <div className={cx('login-modal')}>
         <div className={cx('bar')}></div>
         <div className={cx('content')}>
-          <h2>Login</h2>
-          <div className={cx('form')}>
-            <Input fullWidth big placeholder="email"/>
-            <Input fullWidth big placeholder="password" type="password"/>
-          </div>
+          <h3>Login with email</h3>
+            <div className={cx('form')}>
+              <Input
+                name="email"
+                fullWidth big
+                placeholder="Email"/>
+              <Input
+                name="password"
+                fullWidth big
+                placeholder="Password"
+                type="password"/>
+            </div>
+            <Button
+              flat color="teal"
+              flex padding="0.6rem"
+              className={cx('login')}>Login</Button>
+            <div className={cx('login-foot')}>
+              <TextButton>Forgot password</TextButton>
+              <TextButton right >Signup</TextButton>
+            </div>
+            <div className={cx('separator')}>
+              <div className={cx('or')}>OR</div>
+            </div>
+            <h3>Login with Social Account</h3>
         </div>
       </div>
     </Modal>
